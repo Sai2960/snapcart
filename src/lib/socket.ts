d04fd853acb79b07c5898f3e5f1f90a8d2654124
Sai Chandorkar <saichandorkar06@gmail.com>
2026-04-22 21:08:37 +0530

    fix: socket transport for render

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -6,6 +6,7 @@ export const getSocket = () => {
   if (!socket) {
     socket = io(process.env.NEXT_PUBLIC_SOCKET_SERVER!, {
       autoConnect: false,
+      transports: ["websocket"],
       reconnection: true,
       reconnectionAttempts: 5,
       reconnectionDelay: 1000,
