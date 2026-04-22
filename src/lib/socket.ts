fc3c132d9e9a8e7ccedd376ba7652b4ab4eb86b0
Sai Chandorkar <saichandorkar06@gmail.com>
2026-04-22 21:22:58 +0530

    fix: hardcode socket fallback url

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -4,7 +4,8 @@ let socket: Socket | null = null;
 
 export const getSocket = () => {
   if (!socket) {
-    socket = io(process.env.NEXT_PUBLIC_SOCKET_SERVER!, {
+    const url = process.env.NEXT_PUBLIC_SOCKET_SERVER || "https://socketserver-8s6v.onrender.com";
+    socket = io(url, {
       autoConnect: false,
       transports: ["websocket"],
       reconnection: true,
